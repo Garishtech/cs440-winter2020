2c7b17f838c00749938ee986a213d293c44fc391
taeganwarren <warretaega@gmail.com>
2020-02-06 20:56:02 -0800

    Added switch statement to handle user input

--- assign3/main.cpp
+++ assign3/main.cpp
@@ -2,8 +2,16 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
+
+struct employee {
+    char id[8];
+    char name[200];
+    char bio[500];
+    char manager_id[8];
+};
 
 int main(int argc, char* argv[]) {
 
@@ -28,12 +36,50 @@ int main(int argc, char* argv[]) {
 			if (myFile.good()){
 				// Gets the line
 				while (getline(myFile, line)) {
+					struct employee emp;
 					stringstream ss(line);
 					// Gets the column
+					int counter = 0;
 					while (getline(ss, colname, ',')) {
-						cout << colname << " | ";
+						switch(counter) {
+							case 0:
+								cout << colname;
+								cout << "\n";
+								strcpy(emp.id, colname.c_str());
+								cout << emp.id;
+								cout << "\n";
+								break;
+							case 1:
+								cout << colname;
+								cout << "\n";
+								strcpy(emp.name, colname.c_str());
+								cout << emp.name;
+								cout << "\n";
+								break;
+							case 2:
+								cout << colname;
+								cout << "\n";
+								strcpy(emp.bio, colname.c_str());
+								cout << emp.bio;
+								cout << "\n";
+								break;
+							case 3:
+								cout << colname;
+								cout << "\n";
+								strcpy(emp.manager_id, colname.c_str());
+								cout << emp.manager_id;
+								cout << "\n";
+								break;
+						}
+						cout << emp.id;
+						cout << "\n";
+						cout << emp.name;
+						cout << "\n";
+						cout << emp.bio;
+						cout << "\n";
+						cout << emp.manager_id;
+						cout << "\n";
 					}
-					cout << "\n";
 				}
 			}
 
